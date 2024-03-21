49ef12ed783099898161e74e1cfcd4c92bdd7efa
Mohammad Ashique Usmani <104290715+DreamCoder123@users.noreply.github.com>
2024-03-21 15:27:23 +0530

    Add files via upload

---
+++ New Portolio/README.md
@@ -0,0 +1,25 @@
+# Portolio-new
+
+## Tech Stack
+#### HTML
+#### CSS 
+#### JAVASCRIPT
+  
+## About
+This is My Portfolio Website Which is My solo Project.
+
+## Deploy Link
+Netlify Link 
+[https://funny-churros-0ef475.netlify.app/](https://ashiqueportfolio1.netlify.app/)
+
+
+## How To Use IT
+Just Click the Above Link 
+
+![Screenshot (1074)](https://user-images.githubusercontent.com/104290715/189983792-bdf7acc7-ea6e-4b7c-8cc5-89fa624771af.png)
+
+![Screenshot (1075)](https://user-images.githubusercontent.com/104290715/189983795-5a516f30-4222-43e6-be8e-36f18cb97365.png)
+
+![Screenshot (1076)](https://user-images.githubusercontent.com/104290715/189983784-a308648a-63fe-40ad-996f-b952523a968e.png)
+
+
---
+++ New Portolio/main.js
@@ -0,0 +1,43 @@
+function getEliment(el){
+    return document.querySelector(el)
+}
+
+let All_skills =getEliment(".All_skills")
+let frontend =getEliment(".forntend")
+let backend =getEliment(".backend")
+
+function Allskill(){
+    All_skills.style.display="grid"
+    frontend.style.display="none"
+    backend.style.display="none"
+}
+function Frontend(){
+    All_skills.style.display="none"
+    frontend.style.display="grid"
+    backend.style.display="none"
+}
+function Backend(){
+    All_skills.style.display="none"
+    frontend.style.display="none"
+    backend.style.display="grid"
+}
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  function openFullView(img) {
+    var fullView = document.querySelector('.full-view');
+    fullView.style.display = 'flex';
+    fullView.querySelector('img').src = img.src;
+  }
+  
+  function closeFullView(div) {
+    div.style.display = 'none';
+  }
